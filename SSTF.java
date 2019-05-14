4feb7c22e7bcdc3486a2f0e6eedea61f4384a294
jrosario22 <43249854+jrosario22@users.noreply.github.com>
2019-05-13 23:01:35 -0400

    Add files via upload

---
+++ SSTF.java
@@ -0,0 +1,81 @@
+import java.util.Scanner;
+
+public class SSTF extends node
+{
+		
+	    // Calculate diff of each track with the head position 
+	    public static void calcDiff(int queue[], int head, node diff[])                                         
+	    { 
+	        for (int i = 0; i < diff.length; i++) 
+	            diff[i].distance = Math.abs(queue[i] - head); 
+	    } 
+
+	    // Find minimum distance from head 
+	    public static int findMin(node diff[]) 
+	    { 
+	        int index = -1, minimum = Integer.MAX_VALUE; 
+	        
+	        for (int i = 0; i < diff.length; i++) 
+	        { 
+	            if (!diff[i].accessed && minimum > diff[i].distance) 
+	            { 
+	                minimum = diff[i].distance; 
+	                index = i; 
+	            } 
+	        } 
+	        return index; 
+	    } 
+	  
+	    public static void shortestSeekTimeFirst(int request[], int head) 
+	    { 
+	    	  
+	        int seek_count = 0;  //Count number of seek operation
+	        int[] seek_sequence = new int[request.length + 1]; //Store seq  
+	        node diff[] = new node[request.length]; 
+	        
+	        if (request.length == 0)
+	        {
+	            return;
+	        }
+	                   
+	        // Initialize array 
+	        for (int i = 0; i < diff.length; i++)
+	        {
+	            diff[i] = new node(); 
+	        }
+	          
+	        for (int i = 0; i < request.length; i++) 
+	        { 	              
+	            seek_sequence[i] = head; 
+	            calcDiff(request, head, diff); 	
+	            int index = findMin(diff); 	              
+	            diff[index].accessed = true; 
+	             
+	            seek_count += diff[index].distance; //Increase count
+	              
+	            head = request[index]; //Move head
+	        } 
+	          
+	        seek_sequence[seek_sequence.length - 1] = head;  
+
+	        System.out.println("Total number of seek operations = " + seek_count);                                               
+	        System.out.println("Seek Sequence is"); 
+	          
+	        // print the sequence 
+	        for (int i = 0; i < seek_sequence.length; i++)  
+	            System.out.println(seek_sequence[i]); 
+}
+
+
+	    public static void main(String[] args) 
+	    { 
+    
+	    	
+	
+	    	int arr = scanner.nextInt();
+	
+	    	// request array 
+	    	//int arr[] = { 176, 79, 34, 60, 92, 11, 41, 114 };  
+	    	//shortestSeekTimeFirst(arr, 50); 
+	    }
+}
---
+++ node.java
@@ -0,0 +1,12 @@
+
+public class node 
+{
+	 
+    // represent difference between  
+    // head position and track number 
+    int distance = 0;  
+
+    // true if track has been accessed 
+    boolean accessed = false;  
+	 
+}
